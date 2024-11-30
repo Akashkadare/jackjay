34e959a86c38411003a155ed2c756b6992812741
Akash Babarao Kadare <166395492+Akashkadare@users.noreply.github.com>
2024-11-30 19:37:49 +0530

    file uploaded

---
+++ jackajay/carddata.js
@@ -0,0 +1,5 @@
+const items = [
+    { id: 1, name: "Apple", price: "$1" },
+    { id: 2, name: "Banana", price: "$0.5" },
+    { id: 3, name: "Cherry", price: "$2" }
+  ];
---
+++ jackajay/script.js
@@ -0,0 +1,13 @@
+// var colo=document.getElementsByClassName('container-fluid')
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//           $(colo).css("background" , "blue");
+//         }
+  
+//         else{
+//             $(colo).css("background" , "#333");  	
+//         }
+//     })
+//   })
